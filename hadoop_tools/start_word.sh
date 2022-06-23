f1b717bcaa6511bc1864856c13cfd6d923fa2ff0
ark10806 <ark10806@gmail.com>
2022-06-23 19:49:07 +0900

    rm

--- hadoop_tools/start_word.sh
+++
@@ -1,7 +0,0 @@
-$HADOOP_HOME/bin/stop-all.sh
-$HADOOP_HOME/bin/hadoop namenode -format
-$HADOOP_HOME/bin/start-all.sh
-jps
-
-hadoop fs -put $HADOOP_HOME/conf/hadoop-env.sh conf/hadoop-env.sh
-hadoop jar hadoop-examples-*.jar wordcount /conf/hadoop-env.sh wordcount_output
--- hadoop_tools/stop_go.sh
+++
@@ -1,3 +0,0 @@
-$HADOOP_HOME/bin/stop-all.sh
-hadoop namenode -format
-$HADOOP_HOME/bin/start-all.sh
